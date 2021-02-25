acad45189b1f9a22935aa756577ab04eec8b2e7f
michellecgude <michellecgude@gmail.com>
2021-02-25 13:43:21 -0600

    Had to restart django backend. Removing files commit.

--- wallflower/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import User, Profile, Mood, Meditation, UpliftingContent
-
-admin.site.register([User, Profile, Mood, Meditation, UpliftingContent])
--- wallflower/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WallflowerConfig(AppConfig):
-    name = 'wallflower'
