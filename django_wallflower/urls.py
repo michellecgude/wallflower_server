dfc162d9c084869b3fd44b146076d0bb7c372db5
Michelle Gude <michellecgude@gmail.com>
2021-03-03 23:07:41 -0600

    fixed forward slash in route.

--- django_wallflower/urls.py
+++ django_wallflower/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('usersapi/', include('usersapi.urls')),
     path('userdata/', include('wallflower.urls')), # includes app's urlpatterns
 
