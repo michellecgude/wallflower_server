83e365e4738851574c8d4cc4291e72d1b200dcea
Michelle Gude <michellecgude@gmail.com>
2021-03-03 23:14:40 -0600

    fixing auth route with djoser.

--- django_wallflower/urls.py
+++ django_wallflower/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('djoser.urls')),
-    path('', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('usersapi/', include('usersapi.urls')),
     path('userdata/', include('wallflower.urls')), # includes app's urlpatterns
 
