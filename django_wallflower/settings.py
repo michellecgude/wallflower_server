3342224b52ff80cc0f9196bfbd88d202221e2e18
Michelle Gude <michellecgude@gmail.com>
2021-03-09 20:34:03 -0600

    incorporating blacklist tokens in app.

--- django_wallflower/settings.py
+++ django_wallflower/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'djoser',
 
     'wallflower', # app that holds wallflower content/data
