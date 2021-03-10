df7e1a388ac23c49bed5cdff8526dcc210ca5d4b
Michelle Gude <michellecgude@gmail.com>
2021-03-09 20:40:14 -0600

    creating token views.

--- django_wallflower/urls.py
+++ django_wallflower/urls.py
@@ -2,12 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import LogoutAndBlacklistRefreshTokenForUserView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework_simplejwt import views as jwt_views
+
+from .views import LogoutAndBlacklistRefreshTokenForUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,8 +17,8 @@ urlpatterns = [
     path('account/', include('users.urls')),
     path('auth/', include('djoser.urls')),
 
-    path('jwtoken/obtain/', TokenObtainPairView.as_view(), name='token_create'),  
-    path('jwtoken/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwtoken/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  
+    path('jwtoken/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
      path('blacklistoken/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='token_blacklist'),
     
     path('data/', include('wallflower.urls')), 
