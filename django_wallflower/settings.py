235c8c0a6065deeb8889801f2a8e1f11b7f5d1aa
Michelle Gude <michellecgude@gmail.com>
2021-04-26 12:35:59 -0500

    updated hosts and cors.

--- django_wallflower/settings.py
+++ django_wallflower/settings.py
@@ -40,19 +40,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# ALLOWED_HOSTS = ['.herokuapp.com', 'https://wallflower-api.herokuapp.com', 'http://localhost:3000', 'https://wallflower-app.herokuapp.com'] 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'https://wallflower-api.herokuapp.com', 'http://localhost:3000', 'https://wallflower-app.herokuapp.com'] 
 
-# CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_ALLOW_ALL = True
 
-
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',
-#     'https://wallflower-api.herokuapp.com',
-#     'https://wallflower-app.herokuapp.com',
-#     # eventually add heroku link...
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'https://wallflower-api.herokuapp.com',
+    'https://wallflower-app.herokuapp.com',
+)
 
 CORS_ALLOW_METHODS = [
     'DELETE',
